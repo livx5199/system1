914491d1b7651bce18e82369500f7d730b3b5869
livx5199 <livx5199@stud.kea.dk>
2022-12-02 18:09:23 +0100

    Tickets: Add-ticket-button implemented

--- system-1/src/components/TicketButton.jsx
+++ system-1/src/components/TicketButton.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
+import { useState } from 'react';
 
 function TicketButton(props) {
 
-    function add() {
-        props.addToCart(props);
-     }
+  const [counter, setCounter] = useState(0);
+
+  function add() {
+    setCounter(count => count + 1)
+    props.addToCart(props)
+  }
+  
+  function subtract() {
+    if (counter === 0) {
+      setCounter(count => count)
+    } else {
+      setCounter(count => count - 1)
+    }
+
+  }
     
   return (
-    <button onClick={add}>{props.data.name} ({props.data.price},-)</button>
+    <>
+      <h3>{props.data.name} ({props.data.price},-)</h3>
+      <div className="counter-container">
+      <button onClick={subtract}>-</button>
+      <h4>{counter}</h4>
+        <button onClick={add}>+</button>
+      </div>
+    </>
   )
 }
+
 
 export default TicketButton
